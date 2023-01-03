8f3edb71d2fa56490f0466e4184e73301971a340
leesangjin <tkdwls0628@gmail.com>
2023-01-03 17:22:48 +0900

    first commit

---
+++ src/main/java/com/petspace/dev/DevApplication.java
@@ -0,0 +1,13 @@
+package com.petspace.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevApplication.class, args);
+	}
+
+}
