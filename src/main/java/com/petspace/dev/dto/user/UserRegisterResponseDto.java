5ec0ca2b80abd6d1061cbbcce430576fc50cde0d
Byungwook <woook27@naver.com>
2023-01-05 23:12:35 +0900

    Feat: add UserRegisterResponseDto

---
+++ src/main/java/com/petspace/dev/dto/user/UserRegisterResponseDto.java
@@ -0,0 +1,10 @@
+package com.petspace.dev.dto.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class UserRegisterResponseDto {
+    private Long id;
+}
