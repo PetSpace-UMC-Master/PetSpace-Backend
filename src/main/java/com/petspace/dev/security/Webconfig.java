4fc7b43998a8fec349c6a3bfb429b5d9f354c0b3
tanonkim <robbins4bos@gmail.com>
2023-01-06 13:37:12 +0900

    Feat : Webconfig for CORS and Cookie Setting

---
+++ src/main/java/com/petspace/dev/security/Webconfig.java
@@ -0,0 +1,32 @@
+package com.petspace.dev.security;
+
+import org.apache.tomcat.util.http.Rfc6265CookieProcessor;
+import org.apache.tomcat.util.http.SameSiteCookies;
+import org.springframework.boot.web.embedded.tomcat.TomcatContextCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class Webconfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8088")
+                //클라이언트 로컬 주소. 클라이언트에서 내 서버의 api에 접근 시 허용에 관한 부분. CORS.
+                //2개 이상의 origin에 대해서 허용할 수 있음!
+                .allowedMethods("POST","GET","PUT","DELETE","HEAD","OPTIONS") // 클라이언트에서 요청하는 메소드 허용범위
+                .allowCredentials(true);
+    }
+
+    @Bean
+    public TomcatContextCustomizer sameSiteCookiesConfig() {
+        return context -> {
+            final Rfc6265CookieProcessor cookieProcessor = new Rfc6265CookieProcessor();
+            cookieProcessor.setSameSiteCookies(SameSiteCookies.NONE.getValue());
+            context.setCookieProcessor(cookieProcessor);
+        };
+    }
+}
