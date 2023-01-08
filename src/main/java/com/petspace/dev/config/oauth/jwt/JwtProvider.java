11c55596d44c6fc5feda0c569d6f114bf480f704
leesangjin <tkdwls0628@gmail.com>
2023-01-08 20:22:56 +0900

    Fix: Add @Component

--- src/main/java/com/petspace/dev/config/oauth/jwt/JwtProvider.java
+++ src/main/java/com/petspace/dev/config/oauth/jwt/JwtProvider.java
@@ -8,11 +8,13 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 import java.nio.charset.StandardCharsets;
 import java.util.Date;
 import java.util.Random;
 
+@Component
 @Slf4j
 public class JwtProvider {
 
