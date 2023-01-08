49e1d8afb95f2d05d50f022c26a0a5bcda8999b7
leesangjin <tkdwls0628@gmail.com>
2023-01-08 19:54:10 +0900

    Feat: Create dto for authCode

---
+++ src/main/java/com/petspace/dev/config/oauth/dto/OAuthRequestDto.java
@@ -0,0 +1,11 @@
+package com.petspace.dev.config.oauth.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class OAuthRequestDto {
+
+    private String authCode;
+}
