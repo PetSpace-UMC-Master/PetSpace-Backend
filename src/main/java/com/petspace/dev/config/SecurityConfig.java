85bafe2ccd3586bf0fed6b0907d026073b8d39a6
leesangjin <tkdwls0628@gmail.com>
2023-01-08 18:02:55 +0900

    Feat: Set initial security config

---
+++ src/main/java/com/petspace/dev/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.petspace.dev.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@Slf4j
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/", "/oauth/**")
+                .permitAll();
+
+        return http.build();
+    }
+}
