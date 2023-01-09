672352db529a7337ac01c037c4013f25a0c5868e
leesangjin <tkdwls0628@gmail.com>
2023-01-09 15:36:41 +0900

    Feat: Change securityConfig

--- src/main/java/com/petspace/dev/config/SecurityConfig.java
+++ src/main/java/com/petspace/dev/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,9 +15,11 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
+                .formLogin().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
                 .authorizeRequests()
-                .antMatchers("/", "/oauth/**")
-                .permitAll();
+                .antMatchers("/", "/oauth/**").permitAll();
 
         return http.build();
     }
