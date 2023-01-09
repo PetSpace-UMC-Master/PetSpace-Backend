89403f93c9f2392d984556c3669de725622992ac
leesangjin <tkdwls0628@gmail.com>
2023-01-09 16:59:53 +0900

    Refactor: Change login api for connecting with frontend

--- src/main/java/com/petspace/dev/config/oauth/OAuthController.java
+++ src/main/java/com/petspace/dev/config/oauth/OAuthController.java
@@ -1,13 +1,14 @@
 package com.petspace.dev.config.oauth;
 
-import com.petspace.dev.config.oauth.dto.UserOAuthResponseDto;
+import com.petspace.dev.config.oauth.dto.OAuthRequestDto;
+import com.petspace.dev.config.oauth.dto.OAuthResponseDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,10 +19,12 @@ public class OAuthController {
 
     private final OAuthService oAuthService;
 
-    @GetMapping("/{provider}")
-    public ResponseEntity<UserOAuthResponseDto> KakaoAuthRequest_PathVariable(@PathVariable String provider, @RequestParam String code) {
-        log.info("provider={}, code={}", provider, code);
-        UserOAuthResponseDto loginResponseDto = oAuthService.login(provider, code);
-        return ResponseEntity.ok(loginResponseDto);
+    @PostMapping("/{provider}")
+    public ResponseEntity<OAuthResponseDto> OauthLoginRequest(@PathVariable String provider,
+                                                              @RequestBody OAuthRequestDto requestDto) {
+        String accessToken = requestDto.getAccessToken();
+        log.info("provider={}, accessToken={}", provider, accessToken);
+        OAuthResponseDto oAuthResponseDto = oAuthService.login(provider, accessToken);
+        return ResponseEntity.ok(oAuthResponseDto);
     }
 }
