ec507f69ea0eebcbf75e7ee1d4b2f8c88c02eaf0
leesangjin <tkdwls0628@gmail.com>
2023-01-15 19:49:13 +0900

    Refactor: Remove or add some status

--- src/main/java/com/petspace/dev/util/BaseResponseStatus.java
+++ src/main/java/com/petspace/dev/util/BaseResponseStatus.java
@@ -1,8 +1,10 @@
 package com.petspace.dev.util;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public enum BaseResponseStatus {
 
     /**
@@ -13,25 +15,17 @@ public enum BaseResponseStatus {
     /**
      * 2000 : Request 오류
      */
-    // Common
-    REQUEST_ERROR(false, 2000, "입력값을 확인해주세요."),
+    // Validation 예외
+    INVALID_INPUT(false, 2000, "입력값을 확인해주세요."),
+
+    // JWT 예외
     EMPTY_JWT(false, 2001, "JWT를 입력해주세요."),
     INVALID_JWT(false, 2002, "유효하지 않은 JWT입니다."),
     INVALID_USER_JWT(false, 2003, "권한이 없는 유저의 접근입니다."),
 
-    // user
-    EMPTY_USER_ID(false, 2010, "유저 아이디 값을 확인해주세요."),
-
-    // [POST] /user
-    POST_USER_EMPTY_EMAIL(false, 2011, "이메일을 입력해주세요."),
-    POST_USER_INVALID_EMAIL(false, 2012, "이메일 형식을 확인해주세요."),
-    POST_USER_EXISTS_EMAIL(false, 2013, "중복된 이메일입니다."),
-    POST_USER_EMPTY_NICKNAME(false, 2014, "닉네임을 입력해주세요."),
-    POST_USER_EXISTS_NICKNAME(false, 2015, "중복된 닉네임입니다."),
-    POST_USER_EMPTY_USERNAME(false, 2016, "사용자 이름을 입력해주세요."),
-    POST_USER_EMPTY_BIRTH(false, 2017, "생년월일을 입력해주세요."),
-    POST_USER_EMPTY_PASSWORD(false, 2018, "비밀번호을 입력해주세요."),
-    POST_USER_EMPTY_MARKETING_AGREEMENT(false, 2019, "마케팅 이용 동의 여부를 입력해주세요."),
+    // UserException
+    DUPLICATED_EMAIL(false, 2013, "중복된 이메일입니다."),
+    INVALID_EMAIL_OR_PASSWORD(false, 2014, "이메일 혹은 비밀번호가 잘못되었습니다."),
 
     /**
      * 3000 : Response 오류
@@ -48,10 +42,4 @@ public enum BaseResponseStatus {
     private final boolean isSuccess;
     private final int responseCode;
     private final String responseMessage;
-
-    private BaseResponseStatus(boolean isSuccess, int responseCode, String responseMessage) { //BaseResponseStatus 에서 각 해당하는 코드를 생성자로 맵핑
-        this.isSuccess = isSuccess;
-        this.responseCode = responseCode;
-        this.responseMessage = responseMessage;
-    }
 }
