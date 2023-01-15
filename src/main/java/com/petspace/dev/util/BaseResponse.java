cea3c3df248bbaaa4c45be02d47dfc2c0314a29e
leesangjin <tkdwls0628@gmail.com>
2023-01-15 19:48:26 +0900

    Refactor: Add constructor and inner class for validation exception

--- src/main/java/com/petspace/dev/util/BaseResponse.java
+++ src/main/java/com/petspace/dev/util/BaseResponse.java
@@ -3,23 +3,24 @@ package com.petspace.dev.util;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import org.springframework.validation.FieldError;
 
 import static com.petspace.dev.util.BaseResponseStatus.SUCCESS;
 
 @Getter
-@AllArgsConstructor
 @JsonPropertyOrder({"isSuccess", "responseCode", "responseMessage", "result"})
 public class BaseResponse<T> {
 
     @JsonProperty("isSuccess")
     private final Boolean isSuccess;
-    private final String responseMessage;
-    private final int responseCode;
 
-    @JsonInclude(JsonInclude.Include.NON_NULL)
-    private T result;
+    private final int responseCode;
+    private final String responseMessage;
+
+    @JsonInclude(JsonInclude.Include.NON_EMPTY)
+    private final T result;
 
     // Request Success
     public BaseResponse(T result){
@@ -29,11 +30,35 @@ public class BaseResponse<T> {
         this.result = result;
     }
 
-    // Request Fail
+    // Common Exception
     public BaseResponse(BaseResponseStatus status) {
         this.isSuccess = status.isSuccess();
         this.responseMessage = status.getResponseMessage();
         this.responseCode = status.getResponseCode();
+        this.result = null;
     }
 
+    // TODO 프론트와 협의 후, 만약 에러메시지만 출력되도록 한다면, 이 아래로 모두 지워도 괜찮
+    // Validation Exception
+    public BaseResponse(BaseResponseStatus status, T result) {
+        this.isSuccess = status.isSuccess();
+        this.responseMessage = status.getResponseMessage();
+        this.responseCode = status.getResponseCode();
+        this.result = result;
+    }
+
+    // @Valid 예외 발생시, 여러 예외를 던져줌
+    @Getter
+    @Builder
+    public static class ValidationError {
+        private final String field;
+        private final String message;
+
+        public static BaseResponse.ValidationError of(final FieldError fieldError) {
+            return BaseResponse.ValidationError.builder()
+                    .field(fieldError.getField())
+                    .message(fieldError.getDefaultMessage())
+                    .build();
+        }
+    }
 }
