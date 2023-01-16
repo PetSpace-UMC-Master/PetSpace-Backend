7c377dcc6c77d4663b0a7fef9b12b2bf9d9ecf4f
leesangjin <tkdwls0628@gmail.com>
2023-01-16 12:05:11 +0900

    Refactor: Method segregation

--- src/main/java/com/petspace/dev/util/exception/handler/CustomExceptionHandler.java
+++ src/main/java/com/petspace/dev/util/exception/handler/CustomExceptionHandler.java
@@ -16,19 +16,22 @@ import static com.petspace.dev.util.BaseResponseStatus.INVALID_INPUT;
 @Slf4j
 public class CustomExceptionHandler {
 
-    // @Valid Exception Handler -> 실패시, REQUEST_ERROR Status
     @ExceptionHandler({MethodArgumentNotValidException.class})
     public BaseResponse<Object> handleValidationException(MethodArgumentNotValidException e) {
-        log.warn("bindingResult.hasErrors()");
-        List<BaseResponse.ValidationError> errors =
-                e.getBindingResult().getFieldErrors().stream().map(BaseResponse.ValidationError::of).collect(Collectors.toList());
-        // TODO errors를 담아서 모든 검증 오류를 내보낼지, 혹은 메시지만 내보낼지 프론트와 얘기하기
+        List<BaseResponse.ValidationError> errors = getFieldErrors(e);
         return new BaseResponse<>(INVALID_INPUT, errors);
     }
 
-    // UserException -> 중복된 이메일
     @ExceptionHandler({UserException.class})
     public BaseResponse<Object> handleUserException(UserException e) {
         return new BaseResponse<>(e.getStatus());
+    }
+
+    private List<BaseResponse.ValidationError> getFieldErrors(MethodArgumentNotValidException e) {
+        return e.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(BaseResponse.ValidationError::of)
+                .collect(Collectors.toList());
     }
 }
