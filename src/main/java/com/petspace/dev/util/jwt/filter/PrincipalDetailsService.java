b78fdaa8476573b679683179577186c4ec0c348c
leesangjin <tkdwls0628@gmail.com>
2023-01-17 17:20:27 +0900

    Feat: Implement PrincipalDetailsService

---
+++ src/main/java/com/petspace/dev/util/jwt/filter/PrincipalDetailsService.java
@@ -0,0 +1,28 @@
+package com.petspace.dev.util.jwt.filter;
+
+import com.petspace.dev.domain.User;
+import com.petspace.dev.repository.UserRepository;
+import com.petspace.dev.util.exception.UserException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import static com.petspace.dev.util.BaseResponseStatus.NONE_USER;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class PrincipalDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public PrincipalDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UserException(NONE_USER));
+        log.info("loadUserByUsername, user=[{}][{}]", user.getEmail(), user.getRole());
+        return new PrincipalDetails(user);
+    }
+}
