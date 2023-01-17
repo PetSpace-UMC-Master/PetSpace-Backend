f89e2a0ae49e598ff1f29993bccf60ccea704974
leesangjin <tkdwls0628@gmail.com>
2023-01-17 17:25:42 +0900

    Chore: Change SecurityCofig for implementing jwt-filter

--- src/main/java/com/petspace/dev/config/SecurityConfig.java
+++ src/main/java/com/petspace/dev/config/SecurityConfig.java
@@ -1,23 +1,36 @@
 package com.petspace.dev.config;
 
+import com.petspace.dev.util.jwt.JwtProvider;
+import com.petspace.dev.util.jwt.filter.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 @Slf4j
 public class SecurityConfig {
+
+    private final JwtProvider jwtProvider;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter(JwtProvider jwtProvider) {
+        return new JwtAuthenticationFilter(jwtProvider);
     }
 
     @Bean
@@ -28,7 +41,11 @@ public class SecurityConfig {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
-                .antMatchers("/", "/oauth/**").permitAll();
+                .antMatchers("/", "/oauth/**", "/app/login").permitAll()
+                .antMatchers(HttpMethod.POST, "/app/users").permitAll()
+                .antMatchers("/test").authenticated() // 회원 인증 테스트용
+                .and()
+                .addFilterBefore(jwtAuthenticationFilter(jwtProvider), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
