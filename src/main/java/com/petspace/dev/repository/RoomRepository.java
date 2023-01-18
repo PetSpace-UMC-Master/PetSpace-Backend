4e6bd1d034f7e167ef2aafc90b521f4709501072
Byungwook <woook27@naver.com>
2023-01-18 13:39:06 +0900

    Feat: Sorting using Pageable Object

--- src/main/java/com/petspace/dev/repository/RoomRepository.java
+++ src/main/java/com/petspace/dev/repository/RoomRepository.java
@@ -8,6 +8,10 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface RoomRepository extends JpaRepository<Room, Long> {
-    @Query("SELECT r From Room r WHERE r.status = 'ACTIVE' order by r.id desc")
+    @Query("SELECT r, count(r1.id) as review_count, avg(r2.score) as avg_review_score From Room r " +
+            "left outer join fetch Reservation r1 on (r.id = r1.room.id)" +
+            "left outer join fetch Review r2 on (r1.id = r2.reservation.id) " +
+            "WHERE r.status = 'ACTIVE'" +
+            "group by r.id")
     List<Room> findAllDesc(Pageable pageable);
 }
