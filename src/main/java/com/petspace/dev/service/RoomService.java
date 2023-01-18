854f5b84c55922bbef0b65d69ffd6ff7e72964c0
Byungwook <woook27@naver.com>
2023-01-18 13:45:02 +0900

    Feat: getSortBy that sets the sorting criteria, order

--- src/main/java/com/petspace/dev/service/RoomService.java
+++ src/main/java/com/petspace/dev/service/RoomService.java
@@ -5,6 +5,7 @@ import com.petspace.dev.repository.RoomRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,10 +21,18 @@ public class RoomService {
     private static final int defaultPageSize = 5;
 
     @Transactional(readOnly = true)
-    public List<RoomListResponseDto> findAllDesc(Optional<Integer> page) {
-        Pageable pageable = PageRequest.of(page.orElse(0), defaultPageSize);
+    public List<RoomListResponseDto> findAllDesc(Optional<Integer> page, Optional<String> sortBy, Optional<String> order) {
+        Sort sort = getSortBy(sortBy.orElse("id"), order.orElse("desc"));
+        Pageable pageable = PageRequest.of(page.orElse(0), defaultPageSize, sort);
         return roomRepository.findAllDesc(pageable).stream()
                 .map(RoomListResponseDto::new)
                 .collect(Collectors.toList());
+    }
+
+    public Sort getSortBy(String column, String order) {
+        if (order.equals("ASC")) {
+            return Sort.by(column).ascending().and(Sort.by("id").descending());
+        }
+        return Sort.by(column).descending().and(Sort.by("id").descending());
     }
 }
