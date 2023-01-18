17eccb4ac27f4a2417d33512afb9e27cddadede7
YoonSubLim <yslim37@naver.com>
2023-01-18 12:55:21 +0900

    Refactor: divide getRoomDetail's internal logic into several methods

--- src/main/java/com/petspace/dev/service/RoomService.java
+++ src/main/java/com/petspace/dev/service/RoomService.java
@@ -28,7 +28,6 @@ public class RoomService {
 //    }
 
     @Transactional
-    // TODO 메서드로 나눠 구분하기
     public RoomDetailResponseDto getRoomDetail(Long roomId) {
 
         /**
@@ -36,42 +35,78 @@ public class RoomService {
          */
         Optional<Room> roomList = roomRepository.findById(roomId);
         Room room = roomList.get();
-        // TODO roomId Error Handling
+        // TODO roomId Error Handling -> 'Exception 관련 commit by 상진' pull 이후에 진행.
 
+        // Room 의 Image 리스트에서 URL 만을 List<String> 으로 받아오기
+        List<String> imageUrlList = getRoomImageUrlbyId(room);
 
-        /**
-         * Room 의 Image 리스트에서 URL 만을 List<String> 으로 받아오기
-         */
-        List<String> imageUrlList = room.getRoomImages()
-                .stream().map(RoomImage::getRoomImageUrl)
-                .collect(Collectors.toList());
-
-        /**
-         * Room 의 주소 받아오기. TODO 주소 어느 형식으로 보내줄지 논의 필요
-         */
+        // Room 의 주소 받아오기. TODO 주소 어느 형식으로 보내줄지 논의 필요
         String address = room.getAddress().getRegion() + " " + room.getAddress().getCity() + " " + room.getAddress().getAddressDetail();
 
-        /**
-         * 리뷰 평점 가져오기
-         */
-        double review_rate = room.getReservation()
-                .stream().map(Reservation::getReview)
-                .filter(Objects::nonNull) // Review 가 nonNull 인 경우만
-                .map(Review::getScore)
-                .mapToDouble(n -> n)
-                .average()
-                .orElse(0);
+        // 리뷰 평점 가져오기
+        double review_rate = getRoomReviewAvgRate(room);
 
-        /**
-         * 리뷰 개수 가져오기
-         */
-        long review_count = room.getReservation()
-                .stream().map(Reservation::getReview)
-                .count();
+        // 리뷰 개수 가져오기
+        long review_count = getRoomReviewCount(room);
 
-        /**
-         * 리뷰 미리보기 5개 가져오기
-         */
+        // 리뷰 미리보기 5개 가져오기
+        List<RoomDetailReviewDto> roomDetailReviewDtos = getRoomDetailReviewDtos(room);
+
+        // Room 의 편의시설 받아오기
+        List<RoomDetailFacilityDto> facilityInfoDtos = getRoomDetailFacilityDtos(room);
+
+        return RoomDetailResponseDto
+                .builder()
+                .id(room.getId())
+                .host(room.getUser().getUsername())
+                .roomImageUrl(imageUrlList)
+                .address(address)
+                .name(room.getRoomName())
+                .price(room.getPrice())
+                .maxGuest(room.getMaxGuest())
+                .maxPet(room.getMaxPet())
+                .decription(room.getDescription())
+                .checkinTime(room.getCheckinTime()) // TODO CheckIn 시간형식 논의
+                .checkoutTime(room.getCheckoutTime()) // TODO CheckOut 시간형식 논의
+                .rating(review_rate)
+                .reviewCount(review_count)
+                .reviewPreview(roomDetailReviewDtos)
+                .facilities(facilityInfoDtos)
+                .build();
+    }
+
+
+
+    /**
+     * Room 의 편의시설 받아오기
+     */
+    private List<RoomDetailFacilityDto> getRoomDetailFacilityDtos(Room room) {
+
+        // Facility Entity List 가져오기
+        List<Facility> facilities = room.getRoomFacilities()
+                        .stream().map(RoomFacility::getFacility)
+                        .collect(Collectors.toList());
+
+        // Room 엔티티를 필요한 정보로 DTO 화 하여 List 에 담는다.
+        List<RoomDetailFacilityDto> facilityInfoDtos = new ArrayList<>();
+        for(Facility fc : facilities){
+            facilityInfoDtos.add(
+                    RoomDetailFacilityDto.builder()
+                    .facilityName(fc.getFacilityName())
+                    .facilityImageUrl(fc.getFacilityImageUrl())
+                    .build()
+            );
+        }
+        return facilityInfoDtos;
+    }
+
+
+    /**
+     * 리뷰 미리보기 5개 가져오기
+     */
+    private List<RoomDetailReviewDto> getRoomDetailReviewDtos(Room room) {
+
+        // Review Entity List 가져오기
         List<Review> reviewPreview = room.getReservation()
                 .stream().map(Reservation::getReview)
                 .collect(Collectors.toList());
@@ -97,43 +132,41 @@ public class RoomService {
                 );
             }
         }
+        return roomDetailReviewDtos;
+    }
 
-        /**
-         * Room 의 편의시설 받아오기
-         */
-        List<Facility> facilities = room.getRoomFacilities()
-                        .stream().map(RoomFacility::getFacility)
-                        .collect(Collectors.toList());
+    /**
+     * 리뷰 개수 가져오기
+     */
+    private long getRoomReviewCount(Room room) {
+        long review_count = room.getReservation()
+                .stream().map(Reservation::getReview)
+                .count();
+        return review_count;
+    }
 
-        // Room 엔티티를 필요한 정보로 DTO 화 하여 List 에 담는다.
-        List<RoomDetailFacilityDto> facilityInfoDtos = new ArrayList<>();
-        for(Facility fc : facilities){
-            facilityInfoDtos.add(
-                    RoomDetailFacilityDto.builder()
-                    .facilityName(fc.getFacilityName())
-                    .facilityImageUrl(fc.getFacilityImageUrl())
-                    .build()
-            );
-        }
+    /**
+     * 리뷰 평점 가져오기
+     */
+    private double getRoomReviewAvgRate(Room room) {
+        double review_rate = room.getReservation()
+                .stream().map(Reservation::getReview)
+                .filter(Objects::nonNull) // Review 가 nonNull 인 경우만
+                .map(Review::getScore)
+                .mapToDouble(n -> n)
+                .average()
+                .orElse(0);
+        return review_rate;
+    }
 
-        return RoomDetailResponseDto
-                .builder()
-                .id(room.getId())
-                .host(room.getUser().getUsername())
-                .roomImageUrl(imageUrlList)
-                .address(address)
-                .name(room.getRoomName())
-                .price(room.getPrice())
-                .maxGuest(room.getMaxGuest())
-                .maxPet(room.getMaxPet())
-                .decription(room.getDescription())
-                .checkinTime(room.getCheckinTime()) // TODO CheckIn 시간형식 논의
-                .checkoutTime(room.getCheckoutTime()) // TODO CheckOut 시간형식 논의
-                .rating(review_rate)
-                .reviewCount(review_count)
-                .reviewPreview(roomDetailReviewDtos)
-                .facilities(facilityInfoDtos)
-                .build();
+    /**
+     * Room 의 Image 리스트에서 URL 만을 List<String> 으로 받아오기
+     */
+    private List<String> getRoomImageUrlbyId(Room room) {
+        List<String> imageUrlList = room.getRoomImages()
+                .stream().map(RoomImage::getRoomImageUrl)
+                .collect(Collectors.toList());
+        return imageUrlList;
     }
 
 }
