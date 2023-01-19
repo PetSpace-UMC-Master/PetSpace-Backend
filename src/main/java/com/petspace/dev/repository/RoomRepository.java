e07571a7cd4bf8fc0f1c841937301638fc4b0d5c
Byungwook <woook27@naver.com>
2023-01-19 13:16:07 +0900

    Feat: findAllDescByCategory added

--- src/main/java/com/petspace/dev/repository/RoomRepository.java
+++ src/main/java/com/petspace/dev/repository/RoomRepository.java
@@ -4,6 +4,7 @@ import com.petspace.dev.domain.Room;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -14,4 +15,12 @@ public interface RoomRepository extends JpaRepository<Room, Long> {
             "WHERE r.status = 'ACTIVE'" +
             "group by r.id")
     List<Room> findAllDesc(Pageable pageable);
+
+    @Query("SELECT r, count(r1.id) as review_count, avg(r2.score) as avg_review_score From Room r " +
+            "left outer join fetch Reservation r1 on (r.id = r1.room.id) " +
+            "left outer join fetch Review r2 on (r1.id = r2.reservation.id) " +
+            "left outer join fetch RoomCategory rc on (r.id = rc.room.id)" +
+            "WHERE r.status = 'ACTIVE' and rc.category.id = :id " +
+            "group by r.id")
+    List<Room> findAllDescByCategory(Pageable pageable, @Param("id") Long categoryId);
 }
