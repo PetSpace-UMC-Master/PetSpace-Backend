fe8b9f00a8cd4d79b1317a708ebd781e527eee05
Byungwook <woook27@naver.com>
2023-01-19 15:38:29 +0900

    Refactor: Change column name in query

--- src/main/java/com/petspace/dev/repository/RoomRepository.java
+++ src/main/java/com/petspace/dev/repository/RoomRepository.java
@@ -9,14 +9,14 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface RoomRepository extends JpaRepository<Room, Long> {
-    @Query("SELECT r, count(r1.id) as review_count, avg(r2.score) as avg_review_score From Room r " +
+    @Query("SELECT r, count(r1.id) as review_count, avg(r2.score) as average_review_score From Room r " +
             "left outer join fetch Reservation r1 on (r.id = r1.room.id)" +
             "left outer join fetch Review r2 on (r1.id = r2.reservation.id) " +
             "WHERE r.status = 'ACTIVE'" +
             "group by r.id")
     List<Room> findAllDesc(Pageable pageable);
 
-    @Query("SELECT r, count(r1.id) as review_count, avg(r2.score) as avg_review_score From Room r " +
+    @Query("SELECT r, count(r1.id) as review_count, avg(r2.score) as average_review_score From Room r " +
             "left outer join fetch Reservation r1 on (r.id = r1.room.id) " +
             "left outer join fetch Review r2 on (r1.id = r2.reservation.id) " +
             "left outer join fetch RoomCategory rc on (r.id = rc.room.id)" +
