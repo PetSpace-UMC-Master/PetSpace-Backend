b12e3789db281278fef56e12e46add3e65d128f9
tanonkim <robbins4bos@gmail.com>
2023-01-20 13:30:54 +0900

    Refactor: reflecting jwt

--- src/main/java/com/petspace/dev/controller/ReviewController.java
+++ src/main/java/com/petspace/dev/controller/ReviewController.java
@@ -1,5 +1,6 @@
 package com.petspace.dev.controller;
 
+import com.petspace.dev.domain.user.auth.PrincipalDetails;
 import com.petspace.dev.dto.review.ReviewCreateRequestDto;
 import com.petspace.dev.dto.review.ReviewCreateResponseDto;
 import com.petspace.dev.service.ReviewService;
@@ -9,6 +10,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -30,9 +32,10 @@ public class ReviewController {
             @ApiResponse(responseCode = "2022", description = "score를 입력해주세요.")
     })
     @PostMapping("/reviews")
-    public BaseResponse createReview(@RequestParam("userId") Long userId,
+    public BaseResponse createReview(@AuthenticationPrincipal PrincipalDetails principalDetail,
                                      @RequestParam("reservationId") Long reservationId,
                                      @Valid @ModelAttribute ReviewCreateRequestDto reviewCreateRequestDto) {
+        Long userId = principalDetail.getId();
         ReviewCreateResponseDto createResponseDto = reviewService.save(userId, reservationId, reviewCreateRequestDto);
         log.info("score={}", reviewCreateRequestDto.getScore());
 
