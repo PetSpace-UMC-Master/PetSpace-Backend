463a9eac5d2288815abec76c5e808874b37d0eea
YoonSubLim <yslim37@naver.com>
2023-01-21 14:15:14 +0900

    Feat: add swagger in room-detail api

--- src/main/java/com/petspace/dev/controller/RoomController.java
+++ src/main/java/com/petspace/dev/controller/RoomController.java
@@ -3,10 +3,12 @@ package com.petspace.dev.controller;
 import com.petspace.dev.dto.room.RoomDetailResponseDto;
 import com.petspace.dev.service.RoomService;
 import com.petspace.dev.util.BaseResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,6 +17,11 @@ public class RoomController {
 
     private final RoomService roomService;
 
+    @Operation(summary = "RoomDetail Get", description = "RoomDetail Get API Doc")
+    @ApiResponses({
+            @ApiResponse(responseCode = "1000", description = "요청에 성공하였습니다."),
+            @ApiResponse(responseCode = "2030", description = "존재하지 않는 숙소 정보입니다.")
+    })
     @GetMapping("/app/room/{id}")
     public BaseResponse<RoomDetailResponseDto> getRoomDetail(@PathVariable("id") Long roomId){
         RoomDetailResponseDto roomDetailResponseDto = roomService.getRoomDetail(roomId);
