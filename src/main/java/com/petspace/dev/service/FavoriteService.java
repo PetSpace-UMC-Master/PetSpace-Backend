94183b9b52a39dee70574f5ffebc153b7970a270
leesangjin <tkdwls0628@gmail.com>
2023-01-22 20:53:08 +0900

    Feat: Add favorite click logic in FavoriteService

--- src/main/java/com/petspace/dev/service/FavoriteService.java
+++ src/main/java/com/petspace/dev/service/FavoriteService.java
@@ -1,10 +1,21 @@
 package com.petspace.dev.service;
 
+import com.petspace.dev.domain.Favorite;
+import com.petspace.dev.domain.Room;
+import com.petspace.dev.domain.user.User;
+import com.petspace.dev.dto.favorite.FavoriteClickResponseDto;
 import com.petspace.dev.repository.FavoriteRepository;
+import com.petspace.dev.repository.RoomRepository;
+import com.petspace.dev.repository.UserRepository;
+import com.petspace.dev.util.exception.RoomException;
+import com.petspace.dev.util.exception.UserException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import static com.petspace.dev.util.BaseResponseStatus.NONE_ROOM;
+import static com.petspace.dev.util.BaseResponseStatus.NONE_USER;
 
 @Service
 @RequiredArgsConstructor
@@ -12,5 +23,38 @@ import org.springframework.transaction.annotation.Transactional;
 @Slf4j
 public class FavoriteService {
 
+    private final UserRepository userRepository;
+    private final RoomRepository roomRepository;
     private final FavoriteRepository favoriteRepository;
+
+    @Transactional
+    public FavoriteClickResponseDto clickFavorite(Long userId, Long roomId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new UserException(NONE_USER));
+        Room room = roomRepository.findById(roomId)
+                .orElseThrow(() -> new RoomException(NONE_ROOM));
+
+        log.info("user={}, room={}", user.getEmail(), room.getRoomName());
+        return addOrChangeFavoriteStatus(userId, roomId, user, room);
+    }
+
+    private FavoriteClickResponseDto addOrChangeFavoriteStatus(Long userId, Long roomId, User user, Room room) {
+        Favorite favorite = favoriteRepository.findByUserIdAndRoomId(userId, roomId).orElse(null);
+
+        if (favorite == null) {
+            favorite = Favorite.builder()
+                    .user(user)
+                    .room(room)
+                    .isClicked(true)
+                    .build();
+            favoriteRepository.save(favorite);
+        } else {
+            favorite.clickFavorite();
+        }
+        return FavoriteClickResponseDto.builder()
+                .id(favorite.getId())
+                .roomName(favorite.getRoom().getRoomName())
+                .isClicked(favorite.isClicked())
+                .build();
+    }
 }
