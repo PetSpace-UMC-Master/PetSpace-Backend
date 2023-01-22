19fbf3adc6dd19d4cff4f3b31e4497ec905b1a07
leesangjin <tkdwls0628@gmail.com>
2023-01-22 17:49:59 +0900

    Feat: Set initial favorite controller

---
+++ src/main/java/com/petspace/dev/controller/FavoriteController.java
@@ -0,0 +1,16 @@
+package com.petspace.dev.controller;
+
+import com.petspace.dev.service.FavoriteService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/app")
+@RequiredArgsConstructor
+@Slf4j
+public class FavoriteController {
+
+    private final FavoriteService favoriteService;
+}
