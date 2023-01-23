f40f50dcf146b43e96f18fd6b900869d38303c89
leesangjin <tkdwls0628@gmail.com>
2023-01-23 19:13:17 +0900

    Feat: Implement favorite main page api

--- src/main/java/com/petspace/dev/controller/FavoriteController.java
+++ src/main/java/com/petspace/dev/controller/FavoriteController.java
@@ -1,8 +1,13 @@
 package com.petspace.dev.controller;
 
+import com.petspace.dev.domain.user.auth.PrincipalDetails;
+import com.petspace.dev.dto.favorite.FavoriteRegionsResponseDto;
 import com.petspace.dev.service.FavoriteService;
+import com.petspace.dev.util.BaseResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,4 +18,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class FavoriteController {
 
     private final FavoriteService favoriteService;
+
+    @GetMapping("/favorites")
+    public BaseResponse<FavoriteRegionsResponseDto> showMainPage(@AuthenticationPrincipal PrincipalDetails principalDetails) {
+        Long userId = principalDetails.getId();
+        log.info("user={}", userId);
+        FavoriteRegionsResponseDto regionResponseDto = favoriteService.showRegions(userId);
+        return new BaseResponse<>(regionResponseDto);
+    }
 }
