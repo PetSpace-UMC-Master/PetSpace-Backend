318af5a8311da2484a61546bbcd2eab32f37016a
leesangjin <tkdwls0628@gmail.com>
2023-01-25 12:28:48 +0900

    Feat: Implement showing favorites by region api

--- src/main/java/com/petspace/dev/controller/FavoriteController.java
+++ src/main/java/com/petspace/dev/controller/FavoriteController.java
@@ -1,10 +1,18 @@
 package com.petspace.dev.controller;
 
+import com.petspace.dev.domain.user.auth.PrincipalDetails;
+import com.petspace.dev.dto.favorite.FavoriteResponseDto;
 import com.petspace.dev.service.FavoriteService;
+import com.petspace.dev.util.BaseResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/app")
@@ -13,4 +21,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class FavoriteController {
 
     private final FavoriteService favoriteService;
+
+    @GetMapping("/favorites")
+    public BaseResponse<List<FavoriteResponseDto>> showFavorites(@AuthenticationPrincipal PrincipalDetails principalDetails,
+                                                                 @RequestParam String region) {
+
+        Long userId = principalDetails.getId();
+        List<FavoriteResponseDto> responseDtos = favoriteService.showFavoritesByRegion(userId, region);
+        return new BaseResponse<>(responseDtos);
+    }
 }
