65ff4c8b2077522341d47bec3b4e98ae405948db
leesangjin <tkdwls0628@gmail.com>
2023-01-25 13:16:57 +0900

    Refactor: Change getting all favorites query to fetch join

--- src/main/java/com/petspace/dev/repository/FavoriteRepository.java
+++ src/main/java/com/petspace/dev/repository/FavoriteRepository.java
@@ -2,6 +2,7 @@ package com.petspace.dev.repository;
 
 import com.petspace.dev.domain.Favorite;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -10,5 +11,8 @@ public interface FavoriteRepository extends JpaRepository<Favorite, Long> {
 
     Optional<Favorite> findByUserIdAndRoomId(Long userId, Long roomId);
 
-    List<Favorite> findAllFavoritesByUserId(Long userId);
-}
+    @Query("select f from Favorite f " +
+            "join fetch f.user u " +
+            "join fetch f.room r " +
+            "where u.id = :userId and r.address.region = :region")
+    List<Favorite> findAllFavoritesByUserIdAndRegion(Long userId, String region);}
