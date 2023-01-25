54f917eeb61a57defba137af81890c5af44238ff
leesangjin <tkdwls0628@gmail.com>
2023-01-25 14:03:40 +0900

    Refactor: Move favorite click api from RoomController to FavoriteController

--- src/main/java/com/petspace/dev/controller/FavoriteController.java
+++ src/main/java/com/petspace/dev/controller/FavoriteController.java
@@ -1,6 +1,7 @@
 package com.petspace.dev.controller;
 
 import com.petspace.dev.domain.user.auth.PrincipalDetails;
+import com.petspace.dev.dto.favorite.FavoriteClickResponseDto;
 import com.petspace.dev.dto.favorite.FavoriteResponseDto;
 import com.petspace.dev.service.FavoriteService;
 import com.petspace.dev.util.BaseResponse;
@@ -8,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,5 +31,14 @@ public class FavoriteController {
         Long userId = principalDetails.getId();
         List<FavoriteResponseDto> responseDtos = favoriteService.showFavoritesByRegion(userId, region);
         return new BaseResponse<>(responseDtos);
+    }
+
+    @PostMapping("/favorites")
+    public BaseResponse<FavoriteClickResponseDto> addFavorite(@AuthenticationPrincipal PrincipalDetails principalDetails,
+                                                              @RequestParam Long roomId) {
+        Long userId = principalDetails.getId();
+        log.info("user=[{}][{}]", principalDetails.getId(), principalDetails.getUsername());
+        FavoriteClickResponseDto roomResponseDto = favoriteService.clickFavorite(userId, roomId);
+        return new BaseResponse<>(roomResponseDto);
     }
 }
