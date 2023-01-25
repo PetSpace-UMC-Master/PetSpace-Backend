469cfe102602df4810212844be0a1b84e22c61f0
leesangjin <tkdwls0628@gmail.com>
2023-01-25 12:28:02 +0900

    Feat: Add showing favorites by region logic

--- src/main/java/com/petspace/dev/service/FavoriteService.java
+++ src/main/java/com/petspace/dev/service/FavoriteService.java
@@ -4,6 +4,7 @@ import com.petspace.dev.domain.Favorite;
 import com.petspace.dev.domain.Room;
 import com.petspace.dev.domain.user.User;
 import com.petspace.dev.dto.favorite.FavoriteClickResponseDto;
+import com.petspace.dev.dto.favorite.FavoriteResponseDto;
 import com.petspace.dev.repository.FavoriteRepository;
 import com.petspace.dev.repository.RoomRepository;
 import com.petspace.dev.repository.UserRepository;
@@ -13,6 +14,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static com.petspace.dev.util.BaseResponseStatus.NONE_ROOM;
 import static com.petspace.dev.util.BaseResponseStatus.NONE_USER;
@@ -38,6 +42,20 @@ public class FavoriteService {
 
         log.info("user={}, room={}", user.getEmail(), room.getRoomName());
         return addOrChangeFavoriteStatus(favorite, user, room);
+    }
+
+    public List<FavoriteResponseDto> showFavoritesByRegion(Long userId, String region) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new UserException(NONE_USER));
+        List<Favorite> favorites = favoriteRepository.findAllFavoritesByUserId(userId);
+
+        List<FavoriteResponseDto> favoriteResponseDtos = favorites.stream().filter(Favorite::isClicked)
+                // TODO N+1 문제 발생
+                .filter(favorite -> favorite.getRoom().getAddress().getRegion().equals(region))
+                .map(FavoriteResponseDto::of)
+                .collect(Collectors.toList());
+        log.info("favorites=[{}]", favoriteResponseDtos);
+        return favoriteResponseDtos;
     }
 
     private FavoriteClickResponseDto addOrChangeFavoriteStatus(Favorite favorite, User user, Room room) {
