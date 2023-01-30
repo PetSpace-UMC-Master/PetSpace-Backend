b7d345b723b8fe404ea5f385e8538e6db98cd414
leesangjin <tkdwls0628@gmail.com>
2023-01-30 13:02:45 +0900

    Fix: Modify wrong url

--- src/main/java/com/petspace/dev/config/SecurityConfig.java
+++ src/main/java/com/petspace/dev/config/SecurityConfig.java
@@ -39,7 +39,7 @@ public class SecurityConfig {
                 .and()
                 .authorizeRequests()
                 // TODO 로그인, 회원가입, 방 전체보기, 방 상세보기는 비회원인 상태에서도 가능함, 이후에 추가하기
-                .antMatchers("/", "/oauth/**", "/app/login", "/app/sign-up/**", "/app/room/**", "/app/reviews/**",
+                .antMatchers("/", "/app/oauth/**", "/app/login", "/app/sign-up/**", "/app/room/**", "/app/reviews/**",
                         "/app/token-reissue", "/v3/api-docs", "/swagger*/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
