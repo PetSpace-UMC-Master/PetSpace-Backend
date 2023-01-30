d727032d85e9765cb39465aaaf9222172ccff276
Byungwook <woook27@naver.com>
2023-01-30 13:24:07 +0900

    Refactor: change query table name

--- src/main/java/com/petspace/dev/repository/RoomRepository.java
+++ src/main/java/com/petspace/dev/repository/RoomRepository.java
@@ -13,16 +13,16 @@ public interface RoomRepository extends JpaRepository<Room, Long> {
 
     Optional<Room> findById(Long id);
 
-    @Query("SELECT r, count(r2.id) as review_count, avg(r2.score) as average_review_score From Room r " +
-            "left outer join fetch Reservation r1 on (r.id = r1.room.id)" +
-            "left outer join fetch Review r2 on (r1.id = r2.reservation.id) " +
+    @Query("SELECT r, count(rv.id) as review_count, avg(rv.score) as average_review_score From Room r " +
+            "left outer join fetch Reservation rs on (r.id = rs.room.id)" +
+            "left outer join fetch Review rv on (rs.id = rv.reservation.id) " +
             "WHERE r.status = 'ACTIVE'" +
             "group by r.id")
     List<Room> findAllDesc(Pageable pageable);
 
-    @Query("SELECT r, count(r2.id) as review_count, avg(r2.score) as average_review_score From Room r " +
-            "left outer join fetch Reservation r1 on (r.id = r1.room.id) " +
-            "left outer join fetch Review r2 on (r1.id = r2.reservation.id) " +
+    @Query("SELECT r, count(rv.id) as review_count, avg(rv.score) as average_review_score From Room r " +
+            "left outer join fetch Reservation rs on (r.id = rs.room.id) " +
+            "left outer join fetch Review rv on (rs.id = rv.reservation.id) " +
             "left outer join fetch RoomCategory rc on (r.id = rc.room.id)" +
             "WHERE r.status = 'ACTIVE' and rc.category.id = :id " +
             "group by r.id")
