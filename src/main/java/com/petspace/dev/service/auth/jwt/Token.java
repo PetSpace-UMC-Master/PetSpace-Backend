e0bb9a42584ef6fcaeae02d91ceabbf5f210d063
leesangjin <tkdwls0628@gmail.com>
2023-01-31 16:40:43 +0900

    Feat: Create token VO

---
+++ src/main/java/com/petspace/dev/service/auth/jwt/Token.java
@@ -0,0 +1,13 @@
+package com.petspace.dev.service.auth.jwt;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class Token {
+
+    private String accessToken;
+    private String refreshToken;
+    private long refreshTokenExpiredIn;
+}
