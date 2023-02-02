ca69403e9a36f14663085f152b45d63e81cd47be
leesangjin <tkdwls0628@gmail.com>
2023-02-02 18:45:48 +0900

    Feat: Add fetch join using userId in ReviewRepository

--- src/main/java/com/petspace/dev/repository/ReviewRepository.java
+++ src/main/java/com/petspace/dev/repository/ReviewRepository.java
@@ -6,9 +6,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 
     @Query("SELECT r FROM Review r WHERE r.status='ACTIVE' ORDER BY r.id DESC ")
     List<Review> findAllDesc(Pageable pageable);
+
+    @Query("select r from Review r " +
+            "join fetch r.reservation rs " +
+            "where r.id = :reviewId and rs.user.id = :userId")
+    Optional<Review> findByIdAndUserId(Long reviewId, Long userId);
 }
