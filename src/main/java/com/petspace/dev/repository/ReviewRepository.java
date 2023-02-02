4da957c0165f6e3d2fa010ae257572027ed2cc3c
leesangjin <tkdwls0628@gmail.com>
2023-02-02 23:13:54 +0900

    Refactor: Remove unused method

--- src/main/java/com/petspace/dev/repository/ReviewRepository.java
+++ src/main/java/com/petspace/dev/repository/ReviewRepository.java
@@ -1,7 +1,6 @@
 package com.petspace.dev.repository;
 
 import com.petspace.dev.domain.Review;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -10,12 +9,9 @@ import java.util.Optional;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 
-    @Query("SELECT r FROM Review r WHERE r.status='ACTIVE' ORDER BY r.id DESC ")
-    List<Review> findAllDesc(Pageable pageable);
-
     @Query("select r from Review r " +
             "join fetch r.reservation rs " +
-            "where r.id = :reviewId and rs.user.id = :userId")
+            "where r.status = 'ACTIVE' and r.id = :reviewId and rs.user.id = :userId")
     Optional<Review> findByIdAndUserId(Long reviewId, Long userId);
 
     @Query("select r from Review r " +
