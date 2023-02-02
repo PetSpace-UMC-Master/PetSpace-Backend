c0d99665b2af4ea7d654fd6f546c04b45e7d4b31
leesangjin <tkdwls0628@gmail.com>
2023-02-02 18:24:17 +0900

    Feat: Add method changing entity to dto

--- src/main/java/com/petspace/dev/dto/review/ReviewResponseDto.java
+++ src/main/java/com/petspace/dev/dto/review/ReviewResponseDto.java
@@ -1,5 +1,6 @@
 package com.petspace.dev.dto.review;
 
+import com.petspace.dev.domain.Review;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -10,4 +11,12 @@ public class ReviewResponseDto {
     private Long id;
     private Integer score;
     private String content;
+
+    public static ReviewResponseDto of(Review review) {
+        return ReviewResponseDto.builder()
+                .id(review.getId())
+                .score(review.getScore())
+                .content(review.getContent())
+                .build();
+    }
 }
