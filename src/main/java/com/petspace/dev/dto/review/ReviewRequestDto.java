72848a2373a685a4b3130203ea3fb7d5d58f2262
leesangjin <tkdwls0628@gmail.com>
2023-02-02 20:11:19 +0900

    Feat: Add mapper method in ReviewRequestDto

--- src/main/java/com/petspace/dev/dto/review/ReviewRequestDto.java
+++ src/main/java/com/petspace/dev/dto/review/ReviewRequestDto.java
@@ -1,5 +1,8 @@
 package com.petspace.dev.dto.review;
 
+import com.petspace.dev.domain.Reservation;
+import com.petspace.dev.domain.Review;
+import com.petspace.dev.domain.Status;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.web.multipart.MultipartFile;
@@ -12,4 +15,13 @@ public class ReviewRequestDto {
     private List<MultipartFile> reviewImages = new ArrayList<>();
     private Integer score;
     private String content;
+
+    public Review toEntity(Reservation reservation) {
+        return Review.builder()
+                .reservation(reservation)
+                .status(Status.ACTIVE)
+                .score(score)
+                .content(content)
+                .build();
+    }
 }
