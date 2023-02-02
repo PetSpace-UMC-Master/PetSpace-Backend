1e99cb3c423e13845a216a098c5c1a8d65f6aeac
leesangjin <tkdwls0628@gmail.com>
2023-02-02 23:45:35 +0900

    Feat: Calculate how long has it been since review was created using formatter

--- src/main/java/com/petspace/dev/dto/review/ReviewsResponseDto.java
+++ src/main/java/com/petspace/dev/dto/review/ReviewsResponseDto.java
@@ -3,6 +3,7 @@ package com.petspace.dev.dto.review;
 
 import com.petspace.dev.domain.Review;
 import com.petspace.dev.domain.image.ReviewImage;
+import com.petspace.dev.util.formatter.DayAfterFormatter;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -15,14 +16,17 @@ public class ReviewsResponseDto {
     private String nickName;
     private List<ReviewImage> reviewImage;
     private int score;
+    private String dayAfterCreated;
     private String content;
 
     public static ReviewsResponseDto of(Review review) {
+
         return ReviewsResponseDto.builder()
                 .id(review.getId())
                 .nickName(review.getReservation().getUser().getNickname())
                 .reviewImage(review.getReviewImages())
                 .score(review.getScore())
+                .dayAfterCreated(DayAfterFormatter.formattingDayAfter(review.getCreatedAt()))
                 .content(review.getContent())
                 .build();
     }
