d4cb5ecf58dc3a4b7cf69e57166b43e92779b2e5
leesangjin <tkdwls0628@gmail.com>
2023-02-04 19:42:36 +0900

    Feat: Create dto for favorites response using slice

---
+++ src/main/java/com/petspace/dev/dto/favorite/FavoritesSliceResponseDto.java
@@ -0,0 +1,17 @@
+package com.petspace.dev.dto.favorite;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@Builder
+public class FavoritesSliceResponseDto {
+    List<FavoriteResponseDto> favorites;
+
+    private long page;
+    @JsonProperty(value = "isLast")
+    private Boolean isLast;
+}
