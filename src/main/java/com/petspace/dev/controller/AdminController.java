bcb0687970787831c1afc0ca4898db3e04402510
YoonSubLim <yslim37@naver.com>
2023-02-06 14:59:49 +0900

    Feat: add find all users Admin API

--- src/main/java/com/petspace/dev/controller/AdminController.java
+++ src/main/java/com/petspace/dev/controller/AdminController.java
@@ -9,6 +9,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import com.petspace.dev.domain.user.User;
+import com.petspace.dev.service.AdminService;
+import java.util.List;
 
 import javax.validation.Valid;
 
@@ -19,6 +22,7 @@ import javax.validation.Valid;
 public class AdminController {
 
     private final UserService userService;
+    private final AdminService adminService;
 
     @RequestMapping("")
     public String home(){
@@ -36,6 +40,15 @@ public class AdminController {
     public String createUser(@Valid UserJoinRequestDto userJoinRequestDto, BindingResult result){
         userService.join(userJoinRequestDto);
         return "redirect:/admin";
+    }
+
+
+    /** 회원 조회 **/
+    @GetMapping("/users")
+    public String list(Model model){
+        List<User> users = adminService.findUsers();
+        model.addAttribute("users", users);
+        return "users/userList";
     }
 
 }
--- src/main/java/com/petspace/dev/service/AdminService.java
+++ src/main/java/com/petspace/dev/service/AdminService.java
@@ -1,15 +1,23 @@
 package com.petspace.dev.service;
 
-import com.petspace.dev.dto.admin.RoomCreateRequestDto;
+import com.petspace.dev.domain.user.User;
+import com.petspace.dev.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 @Slf4j
 public class AdminService {
 
+    private final UserRepository userRepository;
 
+    public List<User> findUsers() {
+        return userRepository.findAll();
+    }
 }
