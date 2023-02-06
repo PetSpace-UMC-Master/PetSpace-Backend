4e0cd73e1247538ae7ea609758c9b5799ef713b6
YoonSubLim <yslim37@naver.com>
2023-02-06 14:26:19 +0900

    Feat: add Admin APIs into SecurityConfig

--- src/main/java/com/petspace/dev/config/SecurityConfig.java
+++ src/main/java/com/petspace/dev/config/SecurityConfig.java
@@ -40,7 +40,7 @@ public class SecurityConfig {
                 .authorizeRequests()
                 // TODO 로그인, 회원가입, 방 전체보기, 방 상세보기는 비회원인 상태에서도 가능함, 이후에 추가하기
                 .antMatchers("/", "/app/oauth/**", "/app/login", "/app/sign-up/**", "/app/reviews/**",
-                        "/app/token-reissue", "/app/room/**", "/v3/api-docs", "/swagger*/**", "/app/rooms/**").permitAll()
+                        "/app/token-reissue", "/app/room/**", "/v3/api-docs", "/swagger*/**", "/app/rooms/**", "/admin/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
