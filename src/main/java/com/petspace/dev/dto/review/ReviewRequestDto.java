a6a31bcec0a68edccdd868993bd3ad1f3f564327
leesangjin <tkdwls0628@gmail.com>
2023-02-07 15:01:51 +0900

    Refactor: Removed unused annotation

--- src/main/java/com/petspace/dev/dto/review/ReviewRequestDto.java
+++ src/main/java/com/petspace/dev/dto/review/ReviewRequestDto.java
@@ -3,22 +3,26 @@ package com.petspace.dev.dto.review;
 import com.petspace.dev.domain.Reservation;
 import com.petspace.dev.domain.Review;
 import com.petspace.dev.domain.Status;
-import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.multipart.MultipartFile;
-import java.util.ArrayList;
+
 import java.util.List;
 
 @Getter
-@AllArgsConstructor
+@Slf4j
+@Builder
 public class ReviewRequestDto {
-    private List<MultipartFile> reviewImages = new ArrayList<>();
+    private List<MultipartFile> reviewImages;
     private Integer score;
     private String content;
 
     public Review toEntity(Reservation reservation) {
         return Review.builder()
+                .user(reservation.getUser())
                 .reservation(reservation)
+                .room(reservation.getRoom())
                 .status(Status.ACTIVE)
                 .score(score)
                 .content(content)
