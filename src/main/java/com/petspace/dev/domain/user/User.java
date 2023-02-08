02a27e64451ffe4fde3a1259e01503767bffa91c
leesangjin <tkdwls0628@gmail.com>
2023-02-08 16:22:40 +0900

    Feat: Add profileImage field in user entity

--- src/main/java/com/petspace/dev/domain/user/User.java
+++ src/main/java/com/petspace/dev/domain/user/User.java
@@ -35,6 +35,8 @@ public class User extends BaseTimeEntity {
     @Column(name = "user_id")
     private Long id;
 
+    private String profileImage;
+
     @Column(length = 45)
     private String username;
 
@@ -78,9 +80,10 @@ public class User extends BaseTimeEntity {
     List<Favorite> favorites = new ArrayList<>();
 
     @Builder
-    public User(String username, String nickname, String birth, String email, String password,
+    public User(String profileImage, String username, String nickname, String birth, String email, String password,
                 boolean privacyAgreement, boolean marketingAgreement, boolean hostPermission,
                 OauthProvider oauthProvider, Status status, Role role) {
+        this.profileImage = profileImage;
         this.username = username;
         this.nickname = nickname;
         this.birth = birth;
