d2f6e348110a48ad3dcb323a92003ba3682ad4b0
leesangjin <tkdwls0628@gmail.com>
2023-02-12 17:34:37 +0900

    Refactor: Change review save api from using @ModelAttribute to @RequestParam

--- src/main/java/com/petspace/dev/controller/ReviewController.java
+++ src/main/java/com/petspace/dev/controller/ReviewController.java
@@ -2,9 +2,9 @@ package com.petspace.dev.controller;
 
 import com.petspace.dev.domain.user.auth.PrincipalDetails;
 import com.petspace.dev.dto.review.ReviewDeleteResponseDto;
-import com.petspace.dev.dto.review.ReviewListResponseDto;
 import com.petspace.dev.dto.review.ReviewRequestDto;
 import com.petspace.dev.dto.review.ReviewResponseDto;
+import com.petspace.dev.dto.review.ReviewsResponseDto;
 import com.petspace.dev.dto.review.ReviewsSliceResponseDto;
 import com.petspace.dev.service.ReviewService;
 import com.petspace.dev.util.BaseResponse;
@@ -26,6 +26,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/app")
@@ -35,24 +38,19 @@ public class ReviewController {
 
     private final ReviewService reviewService;
 
-    @Operation(summary = "Review Post", description = "Review Post API Doc")
-    @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "요청에 성공하였습니다.",
-                    content = @Content(schema = @Schema(implementation = ReviewListResponseDto.class))),
-            @ApiResponse(responseCode = "200", description = "해당 사용자가 존재하지 않습니다."),
-            @ApiResponse(responseCode = "200", description = "해당 예약이 존재하지 않습니다."),
-            @ApiResponse(responseCode = "200", description = "score를 입력해주세요.")
-    })
-    @PostMapping( "/reviews")
+    @PostMapping(value = "/reviews")
     public BaseResponse<ReviewResponseDto> createReview(@AuthenticationPrincipal PrincipalDetails principalDetail,
-                                                        @RequestParam("reservationId") Long reservationId,
-                                                        @ModelAttribute ReviewRequestDto reviewCreateRequestDto) {
+                                                          @RequestParam("reservationId") Long reservationId,
+                                                          @RequestParam(value = "reviewImages", required = false) List<MultipartFile> reviewImages,
+                                                          @RequestParam(value = "score", required = false, defaultValue = "0") int score,
+                                                          @RequestParam(value = "content", required = false) String content) {
 
         Long userId = principalDetail.getId();
-        ReviewResponseDto createResponseDto = reviewService.save(userId, reservationId, reviewCreateRequestDto);
-
+        ReviewRequestDto requestDto = ReviewRequestDto.of(reviewImages, score, content);
+        ReviewResponseDto createResponseDto = reviewService.save(userId, reservationId, requestDto);
         return new BaseResponse<>(createResponseDto);
     }
+
 
     @Operation(summary = "Getting All Reviews", description = "Review Read API Doc")
     @ApiResponses({
@@ -71,7 +69,7 @@ public class ReviewController {
     @Operation(summary = "Updating Review", description = "Review Update API Doc")
     @ApiResponses({
             @ApiResponse(responseCode = "200", description = "요청에 성공하였습니다.",
-                    content = @Content(schema = @Schema(implementation = ReviewListResponseDto.class))),
+                    content = @Content(schema = @Schema(implementation = ReviewsResponseDto.class))),
     })
     @PatchMapping("/reviews/{reviewId}")
     public BaseResponse<ReviewResponseDto> updateReview(@AuthenticationPrincipal PrincipalDetails principalDetail,
@@ -87,7 +85,7 @@ public class ReviewController {
     @Operation(summary = "Updating Review", description = "Review Update API Doc")
     @ApiResponses({
             @ApiResponse(responseCode = "200", description = "요청에 성공하였습니다.",
-                    content = @Content(schema = @Schema(implementation = ReviewListResponseDto.class))),
+                    content = @Content(schema = @Schema(implementation = ReviewsResponseDto.class))),
     })
     @DeleteMapping("/reviews/{reviewId}")
     public BaseResponse<ReviewDeleteResponseDto> deleteReview(@AuthenticationPrincipal PrincipalDetails principalDetail,
